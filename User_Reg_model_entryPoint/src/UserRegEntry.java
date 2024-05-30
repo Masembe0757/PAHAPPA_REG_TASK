746935ba27ecd132a10bc178e95d12d0db9fbb17
masembe0757 <sendyj886@gmail.com>
2024-05-30 15:19:19 +0300

    commit with Entry point

---
+++ User_Reg_model_entryPoint/src/UserRegEntry.java
@@ -0,0 +1,15 @@
+public class UserRegEntry{
+    public static void main(String[] args) {
+        System.out.println("Welcome to the user registration system");
+        System.out.println("The following is the allowed functionality");
+        System.out.println("Registering new user");
+        System.out.println("Retrieving all users");
+        System.out.println("Retrieving a user by their user name");
+        System.out.println("Deleting all captured users");
+        System.out.println("Deleting a user by their user name ");
+        System.out.println("Upadting user details ");
+        
+        
+    }
+
+}
