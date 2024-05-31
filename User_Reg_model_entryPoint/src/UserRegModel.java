54f62ecee6926948994081629a850017c21bacfa
masembe0757 <sendyj886@gmail.com>
2024-05-31 12:36:48 +0300

    edited

--- User_Reg_model_entryPoint/src/UserRegModel.java
+++ User_Reg_model_entryPoint/src/UserRegModel.java
@@ -15,59 +15,53 @@ class User{
         this.lastname = lastname;
         this.dob= dob;
     }
+
+    //This method sets users user name
+    public void setUsername(String username){
+        this.username = username;
+
+    }
+    //This method sets users first name
+    public void setFirstName(String firstname){
+        this.firstname = firstname;
+
+    }
+    //This method sets users last  name
+    public void setLastName(String lastname){
+        this.lastname = lastname;
+
+    }
+    //This method sets users date of birth
+    public void setDob(Date dob){
+        this.dob = dob;
+
+    }
+
+
+
+    //This method Gets the user name
+    public String getUsername(){
+        return username;
+    }
+
+    //This method gets users first name
+    public String getFirstName(){
+        return firstname;
+    } 
+
+    //This method gets users Last name
+    public String getLastName(){
+        return lastname;
+    } 
+    //This method sets users date of birth
+    public Date getDob(){
+        return dob;
+    }
 }
 
 public class UserRegModel {
 
-    //Array list to store users
-    static final List<User> usrnam = new ArrayList<>();
-    
-    public static void AddUser(String username, String firstname , String lastname, Date dob){
-        User newUser = new User(username,firstname,lastname,dob);
-        usrnam.add(newUser);
-    }
-    //Method to retrieve all users by traversing through the users list
-    public static void RetrieveAllUsers(){
-        for(User x : usrnam){
-            
-            System.out.println("System has registered the following users");
-            System.out.println(" User "+ x.username+" has names "+x.firstname+" "+ x.lastname+" and has an age of "+ x.dob );
-        }
-    }
-
-
-    //Method to retrieves a single user by traversing through the users list for specific user name
-    public static void returnSingleUser(String uname){
-        for(User x : usrnam){
-        if(x.username.equals(uname)){
-            System.out.println("\n User "+ uname+ " has deatails : full name "+ x.firstname+" "+x.lastname+ " and date of birth"+ x.dob);
-        }
-        else{
-            System.out.println("The user is not in the system");
-        }
-    }
-    }
-    //Method to deletes a single user by traversing through the users list for specific user name
-    public static void deleteUser(String uname){
-        for(User x : usrnam){
-        if(x.username.equals(uname)){
-            System.out.println("\n "+ x.firstname+" "+x.lastname+ " has been deleted");
-            usrnam.remove(x);
-        }
-        else{
-            System.out.println("The user is not in the system");
-        }
-    }
-    }
-    //Method to delete all users by traversing through the users list
-    public static void deleteAllUsers(){
-        if(!usrnam.isEmpty()){
-            usrnam.clear();
-        }
-        else{
-            System.out.println("The database is empty");
-        }
-    }
+   
     
     public static void main(String[] args) {
         
